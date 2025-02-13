232baca368c9c2ab8527270ab5553d83b4f9b9c3
NatalieZaki <rioluscout@gmail.com>
2025-02-13 16:35:57 -0300

    main.py now counts number of letters in doc

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     word_count = count_words(file_contents)
-    print(word_count)
+    character_counts = count_letters(file_contents)
+    for char, count in sorted(character_counts.items()):
+        print(f"'{char}': {count}")
 def count_words(text):
    words = text.split()
-   return len(words)
-
+def count_letters(text):
+    counts = {}
+    for character in text:
+        lower_character = character.lower()
+        if lower_character in counts:
+            counts[lower_character] += 1
+        else:
+            counts[lower_character] = 1
+    return counts
 if __name__ == "__main__":
   main()
