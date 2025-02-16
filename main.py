39d2c8421eafba6e715fbc867cf45eea7079ae23
NatalieZaki <rioluscout@gmail.com>
2025-02-16 14:39:19 -0300

    updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+    print(f"--- Begin report of books/frankenstein.txt ---")
     word_count = count_words(file_contents)
     character_counts = count_letters(file_contents)
-    for char, count in sorted(character_counts.items()):
-        print(f"'{char}': {count}")
+    print(f"{word_count} words found in the document")
+    chars_list = []
+    for char, count in character_counts.items():
+        chars_list.append({"char": char, "count": count})
+    chars_list.sort(reverse=True, key=sort_on)
+    for char_dict in chars_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['count']} times")
+    print("--- End report ---")
 def count_words(text):
    words = text.split()
+   return len(words)
+def sort_on(dict):
+    return dict["count"]
 def count_letters(text):
     counts = {}
     for character in text:
         lower_character = character.lower()
-        if lower_character in counts:
-            counts[lower_character] += 1
-        else:
-            counts[lower_character] = 1
+        if lower_character.isalpha():
+            if lower_character in counts:
+                counts[lower_character] += 1
+            else:
+                counts[lower_character] = 1
     return counts
 if __name__ == "__main__":
   main()
